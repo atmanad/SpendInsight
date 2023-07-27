b98ad863a72ba16494b9cfe61e55107253a34cb7
atmanad <atmanad0001@gmail.com>
2023-07-27 19:06:33 +0530

    Removed firebase

--- spend-insight/src/App.js
+++ spend-insight/src/App.js
@@ -1,7 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Transactions from './pages/Transactions';
-import LoginPage from './pages/Login';
 import UserDetails from './pages/UserDetails';
 import Footer from './components/Footer';
 import Header from './components/Header';
@@ -41,7 +40,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/transactions" element={<Transactions user={user} />} />
-        <Route path="/login" element={<LoginPage />} />
         <Route path="/user" element={<UserDetails/>} />
         <Route exact path="/categories" element={<CategoryManagement user={user} />} />
         <Route exact path="/labels" element={<LabelManagement user={user} />} />
