0635f066ca484f14d54b55befb206e4f06e0f87b
atmanad <atmanad0001@gmail.com>
2023-07-28 13:02:51 +0530

    UPDATE server URL

--- spend-insight/src/api/api.js
+++ spend-insight/src/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3001/api/v1";
+// axios.defaults.baseURL = "http://localhost:3001/api/v1";
+axios.defaults.baseURL = "https://spend-insight-server.vercel.app/api/v1"
 const responseBody = (response) => response;
 
 const apiUrl = "https://spend-insight.us.auth0.com/api/v2";
